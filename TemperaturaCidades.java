1096ace98872768d5c9826be61c68fbcb7b32771
DgMarques-br <dimmoreira2606@gmail.com>
2026-03-18 21:34:55 -0300

    Código atualizado

--- TemperaturaCidades.java
+++ TemperaturaCidades.java
@@ -9,13 +9,27 @@ public class TemperaturaCidades {
 
         double somaAtual = 0;
         double somaAnual = 0;
+        double maiorTemp = tempAtual[0];
+        double menorTemp = tempAtual[0];
+        String cidadeMaisQuente = cidades[0];
+        String cidadeMaisFria = cidades[0];
 
-        System.out.println("Análise das Temperaturas \n");
+        System.out.println("=== Análise das Temperaturas ===\n");
 
         for (int i = 0; i < cidades.length; i++) {
 
             somaAtual += tempAtual[i];
             somaAnual += mediaAnual[i];
+
+            // Encontrar maior e menor temperatura
+            if (tempAtual[i] > maiorTemp) {
+                maiorTemp = tempAtual[i];
+                cidadeMaisQuente = cidades[i];
+            }
+            if (tempAtual[i] < menorTemp) {
+                menorTemp = tempAtual[i];
+                cidadeMaisFria = cidades[i];
+            }
 
             System.out.println("Cidade: " + cidades[i]);
             System.out.println("Temperatura atual: " + tempAtual[i] + "°C");
@@ -36,8 +50,11 @@ public class TemperaturaCidades {
         double mediaAtualGeral = somaAtual / cidades.length;
         double mediaAnualGeral = somaAnual / cidades.length;
 
+        System.out.println("=== Resumo Geral ===");
         System.out.printf("Média atual das cidades: %.2f°C\n", mediaAtualGeral);
         System.out.printf("Média anual das cidades: %.2f°C\n", mediaAnualGeral);
+        System.out.println("Cidade mais quente: " + cidadeMaisQuente + " (" + maiorTemp + "°C)");
+        System.out.println("Cidade mais fria: " + cidadeMaisFria + " (" + menorTemp + "°C)");
     }
 
     public static void sugerirRoupa(double temperatura) {
